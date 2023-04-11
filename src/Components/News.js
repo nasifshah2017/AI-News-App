a7241967fb4d4757ea78f397fce2a972c5bc5fa5
Syed Nasif Ali Shah <nasifshah@ibm.com>
2023-04-11 13:24:22 -0300

    Made consistent

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card } from "react-bootstrap";
 import axios from "axios";
 import config from "../config";
 
