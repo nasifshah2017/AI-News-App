549a90b9b85e76e6be999567adc549dd677f3b32
Syed Nasif Ali Shah <nasifshah@ibm.com>
2023-04-11 13:18:03 -0300

    Resized and added Background

--- src/Components/News.js
+++ src/Components/News.js
@@ -27,24 +27,28 @@ class News extends Component {
         return <h1>Loading...</h1>;
       }
       return (
-        <Container>
-          <Row>
-            {news.map((article) => (
-              <Col md={4} key={article.url}>
-                <Card style={{ marginBottom: "30px" }}>
-                  <Card.Img variant="top" src={article.image} alt="AI" />
-                  <Card.Body>
-                    <Card.Title>{article.title}</Card.Title>
-                    <Card.Text>{article.description}</Card.Text>
-                    <a href={article.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
-                      Read More at <i>{article.source.name}</i>
-                    </a>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        <div style={{ backgroundImage: `url(https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`, backgroundSize: "cover", minHeight: "100vh" }}>
+          <Container>
+            <Row>
+              {news.map((article) => (
+                <Col md={4} key={article.url}>
+                  <Card style={{ marginBottom: "30px", height: "100%" }}>
+                    <div style={{height: "150px", overflow: "hidden"}}>
+                      <Card.Img variant="top" src={article.image} alt="AI" style={{height: "100%", objectFit: "cover"}} />
+                    </div>
+                    <Card.Body>
+                      <Card.Title>{article.title}</Card.Title>
+                      <Card.Text>{article.description}</Card.Text>
+                      <a href={article.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                        Read More at <i>{article.source.name}</i>
+                      </a>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </div>
       );
     }
   }
